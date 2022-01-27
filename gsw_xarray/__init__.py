53a208f793c44c60dcddb8161d578425c89bb709
Andrew Barna <abarna@gmail.com>
2022-01-27 23:16:06 +0000

    Ensure submodules are always wrapped

--- gsw_xarray/__init__.py
+++ gsw_xarray/__init__.py
@@ -1,10 +1,25 @@
 __version__ = "0.1.0"
 
+from importlib import import_module
+
 import gsw
 from ._core import _wrapped_funcs
 
+_compat = {
+    "conversions",
+    "density",
+    "energy",
+    "freezing",
+    "geostrophy",
+    "ice",
+    "stability",
+}
+_compat_modules = {name: import_module(f".{name}", "gsw_xarray") for name in _compat}
+
 # See PEP 562
 def __getattr__(name):
+    if name in _compat:
+        return getattr(_compat[name], name)
     try:
         return _wrapped_funcs[name]
     except KeyError:
@@ -14,6 +29,7 @@ def __getattr__(name):
             raise AttributeError(
                 f"module {__name__} has no attribute {name}"
             ) from error
+
 
 def __dir__():
     return list(sorted(set([*_wrapped_funcs.keys(), *dir(gsw)])))
