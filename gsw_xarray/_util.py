eb7a89ebedf950e774ee4923d167ff801eed1cda
Andrew Barna <abarna@gmail.com>
2022-01-27 23:17:50 +0000

    Run black on new files

--- gsw_xarray/_util.py
+++ gsw_xarray/_util.py
@@ -2,15 +2,20 @@ from importlib import import_module
 
 from ._core import _wrapped_funcs
 
+
 def submodule_attr_compat(submodule_name):
     submodule = import_module(submodule_name)
+
     def getattr_(name):
         # this will raise name error if not in the wrapped implimentation, this is desireable
         default = getattr(submodule, name)
         return _wrapped_funcs.get(name, default)
+
     return getattr_
+
 
 def submodule_dir_compat(submodule_name):
     submodule = import_module(submodule_name)
+
     def dir_(name):
         return dir(submodule)
