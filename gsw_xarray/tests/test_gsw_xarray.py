ebb8d3bd9cd64c205f06a27252c87d4ad91fc6ff
Romain <romain.caneill@yahoo.fr>
2022-02-25 11:32:28 +0100

    change unit

--- gsw_xarray/tests/test_gsw_xarray.py
+++ gsw_xarray/tests/test_gsw_xarray.py
@@ -13,13 +13,13 @@ def test_func_standard(ds):
     sigma0 = gsw.sigma0(SA=ds.SA, CT=ds.CT)
     assert sigma0.attrs["standard_name"] == "sea_water_sigma_t"
     assert sigma0.name == "sigma0"
-    assert sigma0.attrs["units"] == "kg m^-3"
+    assert sigma0.attrs["units"] == "kg/m^3"
 
 
 def test_func_return_tuple(ds):
     (CT_SA, CT_pt) = gsw.CT_first_derivatives(ds.SA, 1)
     assert CT_SA.name == "CT_SA"
-    assert CT_SA.attrs["units"] == "K (g/kg)^-1"
+    assert CT_SA.attrs["units"] == "K/(g/kg)"
 
 
 @pytest.mark.parametrize("gsdh", [0, 1, None])
