352b861ab6c63134c2945d44fda4169b676d80d3
Romain <romain.caneill@yahoo.fr>
2022-02-27 07:55:41 +0100

    _names.py

--- gsw_xarray/_attributes.py
+++ gsw_xarray/_attributes.py
@@ -7,6 +7,21 @@ _func_attrs = {
             "units": "1",
         },
     ),
+    "CT_first_derivatives_wrt_t_exact": {
+        "units": "K/(g/kg)",
+    },
+    "CT_freezing": {
+        "units": "degC",
+    },
+    "CT_freezing_first_derivatives": {
+        "units": "K/(g/kg)",
+    },
+    "CT_freezing_first_derivatives_poly": {
+        "units": "K/(g/kg)",
+    },
+    "CT_freezing_poly": {
+        "units": "degC",
+    },
     "CT_from_enthalpy": {
         "standard_name": "sea_water_conservative_temperature",
         "units": "degC",
@@ -37,9 +52,51 @@ _func_attrs = {
         "units": "degC",
         "reference_scale": "ITS-90",
     },
+    "CT_maxdensity": {
+        "units": "degC",
+    },
+    "CT_second_derivatives": {
+        "units": "K/((g/kg)^2)",
+    },
     "C_from_SP": {
         "standard_name": "sea_water_electrical_conductivity",
         "units": "mS/cm",
+    },
+    "Fdelta": {
+        "units": "1",
+    },
+    "Helmholtz_energy_ice": {
+        "units": "J/kg",
+    },
+    "Hill_ratio_at_SP2": {
+        "units": "",
+    },
+    "IPV_vs_fNsquared_ratio": {
+        "units": "1",
+    },
+    "Nsquared": {
+        "units": "",
+    },
+    "O2sol": {
+        "units": "umol/kg",
+    },
+    "O2sol_SP_pt": {
+        "units": "umol/kg",
+    },
+    "SAAR": {
+        "units": "1",
+    },
+    "SA_freezing_from_CT": {
+        "units": "g/kg",
+    },
+    "SA_freezing_from_CT_poly": {
+        "units": "g/kg",
+    },
+    "SA_freezing_from_t": {
+        "units": "g/kg",
+    },
+    "SA_freezing_from_t_poly": {
+        "units": "g/kg",
     },
     "SA_from_SP": {
         "standard_name": "sea_water_absolute_salinity",
@@ -104,18 +161,255 @@ _func_attrs = {
         "standard_name": "sea_water_preformed_salinity",
         "units": "g/kg",
     },
+    "Turner_Rsubrho": {
+        "units": "arcdeg",
+    },
+    "adiabatic_lapse_rate_from_CT": {
+        "units": "K/Pa",
+    },
+    "adiabatic_lapse_rate_ice": {
+        "units": "K/Pa",
+    },
+    "alpha": {
+        "units": "1/K",
+    },
+    "alpha_on_beta": {
+        "units": "kg g^-1 K^-1",
+    },
+    "alpha_wrt_t_exact": {
+        "units": "1/K",
+    },
+    "alpha_wrt_t_ice": {
+        "units": "1/K",
+    },
+    "beta": {
+        "units": "kg/g",
+    },
+    "beta_const_t_exact": {
+        "units": "kg/g",
+    },
+    "cabbeling": {
+        "units": "1/(K^2)",
+    },
+    "chem_potential_water_ice": {
+        "units": "J/kg",
+    },
+    "chem_potential_water_t_exact": {
+        "units": "J/g",
+    },
+    "cp_ice": {
+        "units": "J/(kg*K)",
+    },
+    "cp_t_exact": {
+        "units": "J/(kg*K)",
+    },
+    "deltaSA_atlas": {
+        "units": "g/kg",
+    },
+    "deltaSA_from_SP": {
+        "units": "g/kg",
+    },
+    "dilution_coefficient_t_exact": {
+        "units": "(J/kg)(kg/g)",
+    },
+    "distance": {
+        "units": "m",
+    },
+    "dynamic_enthalpy": {
+        "units": "J/kg",
+    },
+    "enthalpy": {
+        "units": "J/kg",
+    },
+    "enthalpy_CT_exact": {
+        "units": "J/kg",
+    },
+    "enthalpy_diff": {
+        "units": "J/kg",
+    },
+    "enthalpy_first_derivatives": {
+        "units": "J/(kg (g/kg))",
+    },
+    "enthalpy_first_derivatives_CT_exact": {
+        "units": "J/(kg (g/kg))",
+    },
+    "enthalpy_ice": {
+        "units": "J/kg",
+    },
+    "enthalpy_second_derivatives": {
+        "units": "(J/kg)(g/kg)^-2)",
+    },
+    "enthalpy_second_derivatives_CT_exact": {
+        "units": "J/(kg (g/kg)^2)",
+    },
+    "enthalpy_t_exact": {
+        "units": "J/kg",
+    },
+    "entropy_first_derivatives": {
+        "units": "J/(kg K(g/kg))",
+    },
+    "entropy_from_CT": {
+        "units": "J/(kg*K)",
+    },
+    "entropy_from_pt": {
+        "units": "J/(kg*K)",
+    },
+    "entropy_from_t": {
+        "units": "J/(kg*K)",
+    },
+    "entropy_ice": {
+        "units": "J/(kg*K)",
+    },
+    "entropy_second_derivatives": {
+        "units": "J/(kg K^3)",
+    },
     "f": {
         "standard_name": "coriolis_parameter",
         "units": "radians/s",
     },
+    "frazil_properties": {
+        "units": "g/kg",
+    },
+    "frazil_properties_potential": {
+        "units": "g/kg",
+    },
+    "frazil_properties_potential_poly": {
+        "units": "g/kg",
+    },
+    "frazil_ratios_adiabatic": {
+        "units": "g/(kg K)",
+    },
+    "frazil_ratios_adiabatic_poly": {
+        "units": "g/(kg K)",
+    },
+    "geo_strf_dyn_height": {
+        "units": "m^2/s^2",
+    },
+    "geostrophic_velocity": {
+        "units": "-90 ... +90",
+    },
+    "gibbs_ice_part_t": {
+        "units": "",
+    },
+    "gibbs_ice_pt0": {
+        "units": "",
+    },
+    "gibbs_ice_pt0_pt0": {
+        "units": "",
+    },
+    "grav": {
+        "units": "m/s^2",
+    },
+    "ice_fraction_to_freeze_seawater": {
+        "units": "g/kg",
+    },
+    "indexer": {
+        "units": "",
+    },
+    "internal_energy": {
+        "units": "J/kg",
+    },
+    "internal_energy_ice": {
+        "units": "J/kg",
+    },
+    "kappa": {
+        "units": "1/Pa",
+    },
+    "kappa_const_t_ice": {
+        "units": "1/Pa",
+    },
+    "kappa_ice": {
+        "units": "1/Pa",
+    },
+    "kappa_t_exact": {
+        "units": "1/Pa",
+    },
+    "latentheat_evap_CT": {
+        "units": "J/kg",
+    },
+    "latentheat_evap_t": {
+        "units": "J/kg",
+    },
+    "latentheat_melting": {
+        "units": "J/kg",
+    },
+    "match_args_return": {
+        "units": "",
+    },
+    "melting_ice_SA_CT_ratio": {
+        "units": "",
+    },
+    "melting_ice_SA_CT_ratio_poly": {
+        "units": "",
+    },
+    "melting_ice_equilibrium_SA_CT_ratio": {
+        "units": "g/(kg K)",
+    },
+    "melting_ice_equilibrium_SA_CT_ratio_poly": {
+        "units": "g/(kg K)",
+    },
+    "melting_ice_into_seawater": {
+        "units": "g/kg",
+    },
+    "melting_seaice_SA_CT_ratio": {
+        "units": "g/(kg K)",
+    },
+    "melting_seaice_SA_CT_ratio_poly": {
+        "units": "g/(kg K)",
+    },
+    "melting_seaice_equilibrium_SA_CT_ratio": {
+        "units": "g/(kg K)",
+    },
+    "melting_seaice_equilibrium_SA_CT_ratio_poly": {
+        "units": "g/(kg K)",
+    },
+    "melting_seaice_into_seawater": {
+        "units": "degC",
+    },
     "p_from_z": {
         "standard_name": "sea_water_pressure",
+        "units": "dbar",
+    },
+    "pchip_interp": {
+        "units": "",
+    },
+    "pot_enthalpy_from_pt_ice": {
+        "units": "J/kg",
+    },
+    "pot_enthalpy_from_pt_ice_poly": {
+        "units": "J/kg",
+    },
+    "pot_enthalpy_ice_freezing": {
+        "units": "J/kg",
+    },
+    "pot_enthalpy_ice_freezing_first_derivatives": {
+        "units": "(J/kg)/(g/kg)",
+    },
+    "pot_enthalpy_ice_freezing_first_derivatives_poly": {
+        "units": "(J/kg)/(g/kg)",
+    },
+    "pot_enthalpy_ice_freezing_poly": {
+        "units": "J/kg",
+    },
+    "pot_rho_t_exact": {
+        "units": "kg/m^3",
+    },
+    "pressure_coefficient_ice": {
+        "units": "Pa/K",
+    },
+    "pressure_freezing_CT": {
         "units": "dbar",
     },
     "pt0_from_t": {
         "standard_name": "sea_water_potential_temperature",
         "units": "degC",
         "reference_scale": "ITS-90",
+    },
+    "pt0_from_t_ice": {
+        "units": "degC",
+    },
+    "pt_first_derivatives": {
+        "units": "K/(g/kg)",
     },
     "pt_from_CT": {
         "standard_name": "sea_water_potential_temperature",
@@ -127,35 +421,143 @@ _func_attrs = {
         "units": "degC",
         "reference_scale": "ITS-90",
     },
+    "pt_from_pot_enthalpy_ice": {
+        "units": "degC",
+    },
+    "pt_from_pot_enthalpy_ice_poly": {
+        "units": "degC",
+    },
     "pt_from_t": {
         "standard_name": "sea_water_potential_temperature",
         "units": "degC",
         "reference_scale": "ITS-90",
     },
+    "pt_from_t_ice": {
+        "units": "degC",
+    },
+    "pt_second_derivatives": {
+        "units": "K/((g/kg)^2)",
+    },
     "rho": {
         "standard_name": "sea_water_density",
         "units": "kg m^-3",
+    },
+    "rho_alpha_beta": {
+        "units": "kg/m^3",
+    },
+    "rho_first_derivatives": {
+        "units": "(kg/m^3)(g/kg)^-1",
+    },
+    "rho_first_derivatives_wrt_enthalpy": {
+        "units": "(kg/m^3)(g/kg)^-1 (J/kg)^-1",
+    },
+    "rho_ice": {
+        "units": "kg/m^3",
+    },
+    "rho_second_derivatives": {
+        "units": "(kg/m^3)(g/kg)^-2",
+    },
+    "rho_second_derivatives_wrt_enthalpy": {
+        "units": "(kg/m^3)(g/kg)^-2 (J/kg)^-1",
     },
     "rho_t_exact": {
         "standard_name": "sea_water_density",
         "units": "kg/m^3",
     },
+    "seaice_fraction_to_freeze_seawater": {
+        "units": "",
+    },
     "sigma0": {
         "standard_name": "sea_water_sigma_t",
+        "units": "kg/m^3",
+    },
+    "sigma1": {
+        "units": "kg/m^3",
+    },
+    "sigma2": {
+        "units": "kg/m^3",
+    },
+    "sigma3": {
+        "units": "kg/m^3",
+    },
+    "sigma4": {
         "units": "kg/m^3",
     },
     "sound_speed": {
         "standard_name": "speed_of_sound_in_sea_water",
         "units": "m/s",
     },
+    "sound_speed_ice": {
+        "units": "m/s",
+    },
     "sound_speed_t_exact": {
         "standard_name": "speed_of_sound_in_sea_water",
         "units": "m/s",
+    },
+    "specvol": {
+        "units": "m^3/kg",
+    },
+    "specvol_alpha_beta": {
+        "units": "m^3/kg",
+    },
+    "specvol_anom_standard": {
+        "units": "m^3/kg",
+    },
+    "specvol_first_derivatives": {
+        "units": "(m^3/kg)(g/kg)^-1",
+    },
+    "specvol_first_derivatives_wrt_enthalpy": {
+        "units": "(m^3/kg)(g/kg)^-1 (J/kg)^-1",
+    },
+    "specvol_ice": {
+        "units": "m^3/kg",
+    },
+    "specvol_second_derivatives": {
+        "units": "(m^3/kg)(g/kg)^-2",
+    },
+    "specvol_second_derivatives_wrt_enthalpy": {
+        "units": "(m^3/kg)(g/kg)^-2 (J/kg)^-1",
+    },
+    "specvol_t_exact": {
+        "units": "kg/m^3",
+    },
+    "spiciness0": {
+        "units": "",
+    },
+    "spiciness1": {
+        "units": "",
+    },
+    "spiciness2": {
+        "units": "",
+    },
+    "t90_from_t68": {
+        "units": "degC",
+    },
+    "t_deriv_chem_potential_water_t_exact": {
+        "units": "J/g/C",
+    },
+    "t_freezing": {
+        "units": "degC",
+    },
+    "t_freezing_first_derivatives": {
+        "units": "K/(g/kg)",
+    },
+    "t_freezing_first_derivatives_poly": {
+        "units": "K/(g/kg)",
+    },
+    "t_freezing_poly": {
+        "units": "degC",
     },
     "t_from_CT": {
         "standard_name": "sea_water_temperature",
         "units": "degC",
         "reference_scale": "ITS-90",
+    },
+    "t_from_pt0_ice": {
+        "units": "degC",
+    },
+    "thermobaric": {
+        "units": "1/(K Pa)",
     },
     "z_from_p": {
         "standard_name": "height_above_mean_sea_level",
