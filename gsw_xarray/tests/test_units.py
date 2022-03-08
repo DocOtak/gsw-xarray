877bd94c4c34111a2c97f353de66643b3fd857c0
Andrew Barna <abarna@gmail.com>
2022-03-08 23:59:03 +0000

    Attempt to add cf_units checker

--- gsw_xarray/tests/test_units.py
+++ gsw_xarray/tests/test_units.py
@@ -28,3 +28,18 @@ def test_unit_pint(func_name):
     for a in attrs:
         print(a["units"])
         ureg.Unit(a["units"])
+
+@pytest.mark.parametrize("func_name", gsw_base)
+def test_unit_cf_units(func_name):
+    cf_units = pytest.importorskip("cf_units")
+    if func_name in ["indexer", "match_args_return", "pchip_interp"]:
+        # Internal gsw cookery or non wrapped functions
+        return
+    attrs = _func_attrs[func_name]
+    if isinstance(attrs, dict):
+        attrs = [
+            attrs,
+        ]
+    for a in attrs:
+        print(a["units"])
+        cf_units.Unit(a["units"])
