effa9dca13dd6f262cd68a3ead7d9773f3679f8b
Andrew Barna <abarna@gmail.com>
2022-03-18 02:29:17 +0000

    refactor the gen_attr_table script a bit

--- docs/gen_attr_table.py
+++ docs/gen_attr_table.py
@@ -1,28 +1,37 @@
+from sphinx.util import progress_message
+
 from gsw_xarray._names import _names
 from gsw_xarray._attributes import _func_attrs
-
 
 list_table = ""
 
 
-for name, result_name in _names.items():
-    list_table += f"{name}\n{'-' * len(name)}\n"
-    if isinstance(result_name, tuple):
-        list_table += f"Has {len(result_name)} outputs\n\n"
-        for i, result in enumerate(result_name):
-            list_table += f"**{result}**\n\n"
-            list_table += (
-                f"* standard_name: {_func_attrs[name][i].get('standard_name', '')}\n"
-            )
-            list_table += f"* units: {_func_attrs[name][i].get('units', '')}\n\n"
+def _add_attrs(list_table, attrs, label):
+    if (label_value := attrs.get(label)) is not None:
+        list_table += f"* {label}: ``{label_value}``\n"
 
-    else:
-        list_table += "Has 1 output\n\n"
-        list_table += f"**{result_name}**\n\n"
-        list_table += f"* standard_name: {_func_attrs[name].get('standard_name', '')}\n"
-        list_table += f"* units: {_func_attrs[name].get('units', '')}\n\n"
-    list_table += "\n"
+    return list_table
 
 
-with open("_attr_table.rst", "w", encoding="utf8") as f:
-    f.write(list_table)
+with progress_message("Generating gsw attribute table"):
+    for name, result_name in _names.items():
+        list_table += f"{name}\n{'-' * len(name)}\n"
+        if isinstance(result_name, tuple):
+            list_table += f"Has {len(result_name)} outputs\n\n"
+            for i, result in enumerate(result_name):
+                list_table += f"**{result}**\n\n"
+                attrs = _func_attrs[name][i]
+                list_table = _add_attrs(list_table, attrs, "standard_name")
+                list_table = _add_attrs(list_table, attrs, "units")
+                list_table += "\n"
+
+        else:
+            attrs = _func_attrs[name]
+            list_table += "Has 1 output\n\n"
+            list_table += f"**{result_name}**\n\n"
+            list_table = _add_attrs(list_table, attrs, "standard_name")
+            list_table = _add_attrs(list_table, attrs, "units")
+        list_table += "\n"
+
+    with open("_attr_table.rst", "w", encoding="utf8") as f:
+        f.write(list_table)
