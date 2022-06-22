6128fd9fab56cc19edc2228390688a78739694b0
Romain <romain.caneill@yahoo.fr>
2022-06-22 15:04:41 +0200

    sort

--- gsw_xarray/_arguments.py
+++ gsw_xarray/_arguments.py
@@ -1,41 +1,41 @@
 input_units = {
-    "axis": None,  # int, numpy axis
-    "saturation_fraction": "1",  # saturation_fraction must be between 0 and 1, and the default is 0, air free
-    "pt": "degC",
-    "pt0": "degC",
-    "SA_seaice": "g/kg",
-    "t_seaice": "degC",
-    "w_seaice": "1",  # mass fraction of sea ice, w_seaice must be between 0 and 1.
-    "z": "m",  # while z is positive in the atmosphere, it is NEGATIVE in the ocean.
-    "t": "degC",
-    "p": "dbar",
-    "h": "J/kg",
-    "SP": "1",
+    "C": "mS/cm",
+    "CT": "degC",
+    "Rt": "1",
+    "SA": "g/kg",
     "SA_bulk": "g/kg",
-    "h_bulk": "J/kg",
-    "w_Ih": "1",  # mass fraction of ice, w_Ih must be between 0 and 1.
-    "max_dp": "dbar",  # in geo_strf_dyn_height
-    "interp_method": None,
-    "h_pot_bulk": "J/kg",
+    "SA_seaice": "g/kg",
+    "SK": "parts per thousand, ppt",  # The user will have to have a registry that allows ppt
+    "SP": "1",
+    "SR": "g/kg",
+    "Sstar": "g/kg",
+    "axis": "None",  # int, numpy axis
+    "entropy": "J/(kg*K)",
     "geo_strf": "m^2/s^2",
     "geo_strf_dyn_height": "m^2/s^2",
-    "sea_surface_geopotential": "m^2/s^2",
-    "lon": "degree_east",  # See how to deal with it
-    "lat": "degree_north",  # See how to deal with it
-    "Sstar": "g/kg",
-    "rho": "kg/m^3",
-    "C": "mS/cm",
-    "SK": "parts per thousand, ppt",  # See how to deal with it
-    "SR": "g/kg",
-    "Rt": "1",
-    "p_shallow": "dbar",
+    "h": "J/kg",
+    "h_bulk": "J/kg",
+    "h_pot_bulk": "J/kg",
+    "interp_method": "None",
+    "lat": "degree_north",
+    "lon": "degree_east",
+    "max_dp": "dbar",  # in geo_strf_dyn_height
+    "p": "dbar",
     "p_deep": "dbar",
     "p_ref": "dbar",
-    "t_Ih": "degC",
-    "pt0_ice": "degC",
-    "entropy": "J/(kg*K)",
+    "p_shallow": "dbar",
     "pot_enthalpy_ice": "J/kg",
+    "pt": "degC",
+    "pt0": "degC",
+    "pt0_ice": "degC",
+    "rho": "kg/m^3",
+    "saturation_fraction": "1",  # saturation_fraction must be between 0 and 1, and the default is 0, air free
+    "sea_surface_geopotential": "m^2/s^2",
+    "t": "degC",
     "t68": "degC",
-    "CT": "degC",
-    "SA": "g/kg",
+    "t_Ih": "degC",
+    "t_seaice": "degC",
+    "w_Ih": "1",  # mass fraction of ice, w_Ih must be between 0 and 1.
+    "w_seaice": "1",  # mass fraction of sea ice, w_seaice must be between 0 and 1.
+    "z": "m",  # while z is positive in the atmosphere, it is NEGATIVE in the ocean.
 }
