53cbbbb07ddece83fd44600bcaac0051af63b041
Romain <romain.caneill@yahoo.fr>
2022-07-30 21:36:22 +0200

    autodetect test

--- gsw_xarray/tests/test_dataset.py
+++ gsw_xarray/tests/test_dataset.py
@@ -12,16 +12,16 @@ from .test_imports import gsw_base
 def test_use_only_dataset(ds):
     """Give dataset as argument"""
     sigma0_da = gsw_xarray.sigma0(SA=ds.SA, CT=ds.CT)
-    sigma0_ds = gsw_xarray.sigma0(ds)
+    sigma0_ds = gsw_xarray.sigma0(ds=ds)
     xr.testing.assert_identical(sigma0_ds, sigma0_da)
     
 def test_use_partial_dataset(ds):
     """Give dataset as argument + some dataarrays"""
     sigma0_da = gsw_xarray.sigma0(SA=ds.SA, CT=ds.CT)
-    sigma0_ds = gsw_xarray.sigma0(ds, CT=ds.CT)
+    sigma0_ds = gsw_xarray.sigma0(ds=ds, CT=ds.CT)
     xr.testing.assert_identical(sigma0_ds, sigma0_da)
     # The following case must also work
     ds.CT.attrs == {}
     sigma0_da = gsw_xarray.sigma0(SA=ds.SA, CT=ds.CT)
-    sigma0_ds = gsw_xarray.sigma0(ds, CT=ds.CT)
+    sigma0_ds = gsw_xarray.sigma0(ds.SA, ds=ds)
     xr.testing.assert_identical(sigma0_ds, sigma0_da)
