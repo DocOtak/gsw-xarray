3757e6c3da60abae113337e5ed715ccca77ff267
Romain <romain.caneill@yahoo.fr>
2022-11-23 17:23:30 +0100

    change test for accessor (test will fail now)

--- gsw_xarray/tests/test_dataset.py
+++ gsw_xarray/tests/test_dataset.py
@@ -12,17 +12,18 @@ from .test_imports import gsw_base
 def test_use_only_dataset(ds):
     """Give dataset as argument"""
     sigma0_da = gsw_xarray.sigma0(SA=ds.SA, CT=ds.CT)
-    sigma0_ds = gsw_xarray.sigma0(ds=ds)
+    sigma0_ds = ds.gsw.sigma0()
     xr.testing.assert_identical(sigma0_ds, sigma0_da)
 
 
 def test_use_partial_dataset(ds):
     """Give dataset as argument + some dataarrays"""
-    sigma0_da = gsw_xarray.sigma0(SA=ds.SA, CT=ds.CT)
-    sigma0_ds = gsw_xarray.sigma0(ds=ds, CT=ds.CT)
+    sigma0_da = ds.gsw.sigma0(SA=ds.SA, CT=ds.CT)
+    sigma0_ds = ds.gsw.sigma0(CT=ds.CT)
     xr.testing.assert_identical(sigma0_ds, sigma0_da)
     # The following case must also work
-    ds.CT.attrs == {}
-    sigma0_da = gsw_xarray.sigma0(SA=ds.SA, CT=ds.CT)
-    sigma0_ds = gsw_xarray.sigma0(ds.SA, ds=ds)
+    ds["SA2"] = ds["SA"].copy(deep=True)
+    sigma0_ds = ds.gsw.sigma0(ds.SA)
     xr.testing.assert_identical(sigma0_ds, sigma0_da)
+    # Must raise an error (multiple values for SA)
+    ds.gsw.sigma0()
