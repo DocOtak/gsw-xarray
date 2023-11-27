d17f98b9c5b3e47fa08f78ced29d2d908a77db59
Romain Caneill <romain.caneill@yahoo.fr>
2023-11-27 10:46:18 +0000

    test

--- gsw_xarray/conversions.py
+++ gsw_xarray/conversions.py
@@ -1,5 +1,5 @@
 from ._util import submodule_attr_compat, submodule_dir_compat, submodule_all_compat
 
-__getattr__ = submodule_attr_compat("gsw.conversions")
+__getattr__ =  submodule_attr_compat("gsw.conversions")
 __dir__ = submodule_dir_compat("gsw.conversions")
 __all__ = submodule_all_compat("gsw.conversions")
