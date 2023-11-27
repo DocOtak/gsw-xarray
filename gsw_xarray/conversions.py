bd8cc33bf2a2536dffc14de24168a3aa9b519380
Romain Caneill <romain.caneill@yahoo.fr>
2023-11-27 10:50:17 +0000

    flake should complain

--- gsw_xarray/conversions.py
+++ gsw_xarray/conversions.py
@@ -3,3 +3,11 @@ from ._util import submodule_attr_compat, submodule_dir_compat, submodule_all_co
 __getattr__ =  submodule_attr_compat("gsw.conversions")
 __dir__ = submodule_dir_compat("gsw.conversions")
 __all__ = submodule_all_compat("gsw.conversions")
+
+myvar = 0
+myvar = 1
+
+
+t      = 42
+
+import gsw
