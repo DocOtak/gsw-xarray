51075551f875212f80b6da2060eafac16103afa2
Romain Caneill <romain.caneill@yahoo.fr>
2024-05-28 11:05:30 +0200

    Update __init__.py

--- gsw_xarray/__init__.py
+++ gsw_xarray/__init__.py
@@ -7,15 +7,15 @@ import gsw_xarray._accessor  # noqa: F401
 from ._core import _wrapped_funcs
 from ._options import (  # noqa: F401
     get_options,
-    set_cf_name_preference,
     set_non_cf_name,
+    set_cf_name_preference,
     set_options,
 )
 from ._util import _compat, get_attribute
 from ._util_module import submodule_all_compat
 
 gsw_xarray_specific_functions = [
-    "set_options",
+    'set_options',
     "get_options",
     "set_non_cf_name",
     "set_cf_name_preference",
